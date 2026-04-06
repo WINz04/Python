da94344c3b6f89030992940f2f917f6f1eeb7afc
WIN <72561281+WINz04@users.noreply.github.com>
2026-04-06 23:08:52 +0700

    Update System Rekomendasi.py

--- System Rekomendasi.py
+++ System Rekomendasi.py
@@ -1,4 +1,3 @@
-# libary
 import wx
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,16 +8,13 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
 
 
-# Aplikasi
-class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
+class RecommendationApp(wx.Frame): 
     def __init__(self, parent, title):
-        #ukurang/size dari aplikasi
         super(RecommendationApp, self).__init__(parent, title=title, size=(800, 600))
 
         self.panel = wx.Panel(self)
         self.vbox = wx.BoxSizer(wx.VERTICAL)
 
-        #nama/ judul dari aplikasi sistem rekomendasi
         self.st1 = wx.StaticText(self.panel, label='Rekomendasi Tempat Wisata di Jakarta', style=wx.ALIGN_CENTER)
         font = self.st1.GetFont()
         font.PointSize += 10
@@ -26,12 +22,10 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
         self.vbox.Add(self.st1, flag=wx.ALL | wx.EXPAND, border=15)
         self.hbox = wx.BoxSizer(wx.HORIZONTAL)
 
-        #button/tombol Input data (csv)
         self.load_btn = wx.Button(self.panel, label='Input Data')
         self.hbox.Add(self.load_btn, flag=wx.EXPAND | wx.ALL, border=10)
         self.load_btn.Bind(wx.EVT_BUTTON, self.on_load)
 
-        #button/tombol untuk tampilkan data asli tempat wisata di jakarta
         self.display_data_btn = wx.Button(self.panel, label='Tampilkan Data')
         self.hbox.Add(self.display_data_btn, flag=wx.EXPAND | wx.ALL, border=10)
         self.display_data_btn.Bind(wx.EVT_BUTTON, self.display_data)
@@ -67,8 +61,6 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
         self.df = None
         self.user_similarity_matrix = None
 
-
-    #function dari input data
     def on_load(self, event):
         with wx.FileDialog(self, "Input Data", wildcard="CSV files (*.csv)|*.csv",
                            style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
@@ -82,8 +74,6 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
             except Exception as e:
                 wx.LogError(f"Tidak dapat memuat file '{path}'. Kesalahan: {e}")
 
-
-    #function preprocesing data
     def preprocess_data(self):
         self.df['Harga'] = self.df['Harga'].replace('[Rp,.]', '', regex=True).astype(float)
         self.df.dropna(inplace=True)
@@ -92,26 +82,20 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
         df_scaled = pd.DataFrame(scaled_features, columns=['Scaled_Rating', 'Scaled_Harga'])
         self.user_similarity_matrix = cosine_similarity(df_scaled)
 
-
-    #tampilan pesa error ketika data belum dimasukan /input
     def display_data(self, event):
         if self.df is not None:
             self.result_text.SetValue(self.df.to_string(index=False))
         else:
             wx.MessageBox('Muat data terlebih dahulu!', 'Info', wx.OK | wx.ICON_INFORMATION)
 
-
-    #algoritma collaborative filltering
     def get_collaborative_filtering_recommendations(self, place_index, num_recommendations=5):
         if place_index >= len(self.user_similarity_matrix):
-            return pd.DataFrame()  # Kembalikan DataFrame kosong jika terjadi kesalahan indeks
-
+            return pd.DataFrame()  
         place_similarity_scores = self.user_similarity_matrix[place_index]
         similar_places_indices = place_similarity_scores.argsort()[::-1][1:num_recommendations + 1]
         recommended_places = self.df.iloc[similar_places_indices]
         return recommended_places
 
-    #get rekomendasi
     def get_recommendations(self, event):
         if self.df is not None and self.user_similarity_matrix is not None:
             try:
@@ -124,10 +108,7 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
         else:
             wx.MessageBox('Muat data terlebih dahulu!', 'Info', wx.OK | wx.ICON_INFORMATION)
 
-
-    #tampilkan ahsil rekomendasi
     def display_recommendations(self, recommendations):
-        # Hapus kolom 'Review' dari DataFrame recommendations jika ada
         if 'Review' in recommendations.columns:
             recommendations = recommendations.drop(columns=['Review'])
 
@@ -147,7 +128,6 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
         self.result_text.SetValue(table_output)
 
 
-    #visual rekomendasi
     def plot_recommendations(self, recommendations):
         plt.figure(figsize=(8, 6))
         bars = plt.barh(recommendations['Nama_Tempat'], recommendations['Rating'], color='green')
@@ -161,38 +141,37 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
         plt.show()
 
 
-    #tampilan rekomendasi harga rendah
     def plot_low_price_high_rating(self, event):
         if self.df is not None:
             low_price_high_rating = self.df[
                 (self.df['Harga'] < self.df['Harga'].median()) & (self.df['Rating'] > self.df['Rating'].median())]
-            low_price_high_rating_top10 = low_price_high_rating.head(10)  # Ambil 10 teratas
-            plt.figure(figsize=(8, 6))  # Ukuran horizontal
+            low_price_high_rating_top10 = low_price_high_rating.head(10)  
+            plt.figure(figsize=(8, 6))  
             bars = plt.barh(low_price_high_rating_top10['Nama_Tempat'], low_price_high_rating_top10['Rating'], color='blue')
             plt.xlabel('Rating')
             plt.ylabel('Nama Tempat')
             plt.title('Low Budget (Top 10)')
-            plt.gca().invert_yaxis()  # Balik urutan dari atas ke bawah
-            plt.subplots_adjust(left=0.3)  # Sesuaikan margin kiri untuk menampilkan nama tempat
+            plt.gca().invert_yaxis()  
+            plt.subplots_adjust(left=0.3)  
             for bar in bars:
                 plt.text(bar.get_width(), bar.get_y() + bar.get_height() / 2, f'{bar.get_width():.2f}', va='center')
             plt.show()
         else:
             wx.MessageBox('Muat data terlebih dahulu!', 'Info', wx.OK | wx.ICON_INFORMATION)
 
-    #tampilan rekomendasi harga tinggi
+
     def plot_high_price_high_rating(self, event):
         if self.df is not None:
             high_price_high_rating = self.df[
                 (self.df['Harga'] > self.df['Harga'].median()) & (self.df['Rating'] > self.df['Rating'].median())]
-            high_price_high_rating_top10 = high_price_high_rating.head(10)  # Ambil 10 teratas
-            plt.figure(figsize=(8, 6))  # Ukuran horizontal
+            high_price_high_rating_top10 = high_price_high_rating.head(10)  
+            plt.figure(figsize=(8, 6))  
             bars = plt.barh(high_price_high_rating_top10['Nama_Tempat'], high_price_high_rating_top10['Rating'], color='red')
             plt.xlabel('Rating')
             plt.ylabel('Nama Tempat')
             plt.title('High Budget (Top 10)')
-            plt.gca().invert_yaxis()  # Balik urutan dari atas ke bawah
-            plt.subplots_adjust(left=0.3)  # Sesuaikan margin kiri untuk menampilkan nama tempat
+            plt.gca().invert_yaxis()  
+            plt.subplots_adjust(left=0.3)  
             for bar in bars:
                 plt.text(bar.get_width(), bar.get_y() + bar.get_height() / 2, f'{bar.get_width():.2f}', va='center')
             plt.show()
@@ -201,7 +180,6 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
 
     def clear_output(self, event):
         self.result_text.Clear()
-
 
     #evaluasi model
     def evaluate_model(self):
@@ -218,18 +196,16 @@ class RecommendationApp(wx.Frame): #menggunakan WxPyhton sebagai GUI
                     true_ratings.append(row['Rating'])
                     predicted_ratings.append(predicted_places.iloc[0]['Rating'])
             except IndexError:
-                continue  # Lewati jika terjadi kesalahan indeks
+                continue  
 
         mae = mean_absolute_error(true_ratings, predicted_ratings)
-        rmse = np.sqrt(mean_squared_error(true_ratings, predicted_ratings))  # Hitung RMSE dengan cara yang baru
+        rmse = np.sqrt(mean_squared_error(true_ratings, predicted_ratings))  
 
         self.result_text.SetValue(f'Mean Absolute Error (MAE): {mae}\nRoot Mean Squared Error (RMSE): {rmse}')
 
     def on_evaluate(self, event):
         if self.df is not None and self.user_similarity_matrix is not None:
             self.evaluate_model()
-        # else:
-        #     wx.MessageBox('Muat data terlebih dahulu!', 'Info', wx.OK | wx.ICON_INFORMATION)
 
 
 def main():
@@ -242,4 +218,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-#selesai deng
